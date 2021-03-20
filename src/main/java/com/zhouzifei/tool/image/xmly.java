d01aa5b3ab5c2bea50da6b4cbd7dad5ed77a7785
周子斐 <17611555590@163.com>
2021-03-20 12:27:19 +0800

    修改视频失败

--- src/main/java/com/zhouzifei/tool/image/xmly.java
+++ src/main/java/com/zhouzifei/tool/image/xmly.java
@@ -20,6 +20,7 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 import org.junit.Test;
 import org.springframework.data.redis.core.script.ReactiveScriptExecutor;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
 import java.net.HttpURLConnection;
@@ -35,6 +36,29 @@ public class xmly {
     //@Test
     public static String pic(String tsUrl){
         final InputStream inputStream = FileUtil.getInputStreamByUrl(tsUrl, "");
+        if(null == inputStream){
+            throw new RuntimeException();
+        }
+        String result = post("https://upload.ximalaya.com/dtres/headerThumb/upload"
+                , "_xmLog=h5&2872be80-f5d5-4747-9b75-cb9e34503cf0&2.2.17; Hm_lvt_4a7d8ec50cfd6af753c4f8aee3425070=1612664140; trackType=web; fds_otp=8172383124598533914; 1&remember_me=y; 1&_token=296993092&6B62AF70140N62AD80A28DA7DB1479A11D32B64352BEB4ECBC37887968F72543742F4606E28F13M5F3014F4AE8BE20_; 1_l_flag=296993092&6B62AF70140N62AD80A28DA7DB1479A11D32B64352BEB4ECBC37887968F72543742F4606E28F13M5F3014F4AE8BE20__2021-02-0710:16:26; x_xmly_traffic=utm_source%253A%2526utm_medium%253A%2526utm_campaign%253A%2526utm_content%253A%2526utm_term%253A%2526utm_from%253A; Hm_lpvt_4a7d8ec50cfd6af753c4f8aee3425070=1612664187"
+                , inputStream);
+        final JSONObject parse = JSONObject.parseObject(result);
+        if(null == parse){
+            throw new RuntimeException();
+        }
+        final Object data = parse.get("data");
+        if(null == data){
+            throw new RuntimeException();
+        }
+        final Object url = ((JSONArray) data).get(0);
+        if(null == url){
+            throw new RuntimeException();
+        }
+        final Object url1 = ((JSONObject) url).get("url");
+        return (String) url1;
+    }
+    public static String filePic(File file) throws IOException {
+        final InputStream inputStream = new FileInputStream(file);
         if(null == inputStream){
             throw new RuntimeException();
         }
